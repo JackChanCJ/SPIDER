3518febea6d1e4c30a51261ca90fa62551b23a2c
JackChanCJ <408467753@qq.com>
2017-12-10 14:37:05 +0800

    1、能够正常爬取所有的文章 使用方法： 1、先执行BBK1中的代码，获取所有网易博客中的动态部分 2、把BBK1中获取的动态部分，放到list中，再执行BBK2，BBK2会创建“博客文件”文件夹，按照BBK2中的规则爬取相关数据并以TXT格式的方式全部存放在博客文件中 3、BBK2中失败的list部分会更新至faillist.txt，循环反复执行前面的动作，直至faillist中的list全部成功。

---
+++ 163.com/MUMU/BBK1.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+__author__ = 'JACK_CHAN'
+import urllib.request
+import urllib.response
+import urllib.parse
+import re
+
+# # 向服务器发送一个get请求
+# url = 'http://yyyyy330.blog.163.com/blog/#m=0'
+# # 创建一个请求
+# r = urllib.request.Request(url)
+# r.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')
+# req = urllib.request.urlopen(url)
+# print (req.status)
+# b = req.read()
+# BBK = b.decode('gbk')
+# print (BBK)
+# # print (re.findall('title="阅读全文" target="_blank" href=(.*?)/">', BBK))
+
+
+"""
+http://api.blog.163.com/yyyyy330/dwr/call/plaincall/BlogBeanNew.getBlogs.dwr
+Request Method:POST
+Content-Type:text/plain
+
+设置参数，除了c0-param0、c0-param1和c0-param2外都一样。
+c0-param0 ：博主的userId，例如yyyyy330的userId是“134612310”
+c0-param1 ：返回博客数据的起始项，从0开始
+c0-param2 ：一次返回博客的数量，最大值好像是500，具体多少我没有完全去试，600肯定不行，我一般设置500，600以上就不返回数据了。
+如果一个博主写了超过500篇博客，那就可以分多次请求，只要合理设置c0-param1和c0-param2就可以。
+
+"""
+username = 'yyyyy330'
+userID = '134612310'
+url = 'http://api.blog.163.com/' + username + '/dwr/call/plaincall/BlogBeanNew.getBlogs.dwr'
+
+post = urllib.request.Request(url)
+post.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')
+post.add_header('Content-Type', 'text/plain')
+post.add_header('Referer', 'http://api.blog.163.com/crossdomain.html?t=20100205')
+
+paraDict = {
+    'callCount'      :   '1',
+    'scriptSessionId':   '${scriptSessionId}187',
+    'c0-scriptName'  :   'BlogBeanNew',
+    'c0-methodName'  :   'getBlogs',
+    'c0-id'          :   '0',
+    'c0-param0'      :   '134612310',
+    'c0-param1'      :   '0',
+    'c0-param2'      :   '20',
+    'batchId'        :   '1'
+}
+
+paraDict['c0-param0'] = "number:" + userID
+paraDict['c0-param1'] = "number:" + str(0)
+paraDict['c0-param2'] = "number:" + str(500)
+data = urllib.parse.urlencode(paraDict)
+data = data.encode('utf-8')
+req = urllib.request.urlopen(post, data)
+bbk = req.read().decode('utf-8')
+batchID = re.findall('permaSerial="(.*?)"', bbk)
+print ('取出来的长度:', len(batchID))
+
+
+"""
+拼接链接,循环访问并取出标题即文本
+http://yyyyy330.blog.163.com/blog/static/134612310201612375310693/
+'http://' + username + '.blog.163.com/blog/static/' + batchID
+"""
+new_batch = []
+[new_batch.append(i) for i in batchID if not i in new_batch]
+print ('处理之后的长度：', len(new_batch))
+print (new_batch)
+
+
+
+
---
+++ 163.com/__init__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+__author__ = 'JACK_CHAN'
+
+from pages.basePage import Page
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
