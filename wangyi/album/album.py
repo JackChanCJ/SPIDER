ef1376a9884002e8cff32f1d81717f0089274ffc
JackChanCJ <408467753@qq.com>
2018-03-02 14:49:44 +0800

    1、crrent_album_pid获取相册的pid

--- wangyi/album/album.py
+++ wangyi/album/album.py
@@ -12,6 +12,27 @@ wangyiboke_address = 'http://yyyyy330.blog.163.com/'
     Request Method:GET
     Status Code:200 OK (from disk cache)
     Remote Address:123.53.139.232:80
+    self.text获取的数据格式
+{
+    id: 193186286,
+    name: '林中漫步',
+    s: 3,
+    desc: '',
+    st: 5,
+    au: 2,
+    count: 15,
+    t: 1260883742140,
+    ut: 1263138278365,
+    cvid: 5673060057,
+    curl: '2/ZBaHoMf-NHjwV6xlq7Yr3A==/3025855999641186443.jpg',
+    surl: '2/Q2yQGWF9f9Wl-kZ2QHAbCw==/3025855999641186446.jpg',
+    lurl: '0/0-YOfu2Y7j3dYKKznL0pAw==/1766255478861007467.jpg',
+    dmt: 2106086069,
+    alc: true,
+    kw: '',
+    purl: ''
+},
+
 """
 
 
@@ -38,9 +59,7 @@ class album(wangyiboke):
 
     def get_album_aid(self):
         # self.album_id 正则处理self.text，获取相册的id
-        self.album_aid = re.findall('\{id: (.*?),name', self.text)
-        # print("相册id的个数：%s" % len(self.album_id))
-        # print(self.album_id)
+        self.album_aid = re.findall("\{id:(.*?),name:'", self.text)
         return self.album_aid
 
     def get_album_url(self):
@@ -60,17 +79,10 @@ class album(wangyiboke):
             # print (i, id.index(i) + 1)
             album_url = 'http://%s.blog.163.com/album/#m=1&aid=%s&p=1' % (self.username, i)
             self.album_url.append(album_url)
-        # print("相册url的长度：%s" % len(self.album_url))
-        # print(self.album_url)
-
-        # idname = dict(zip(id, name))
-        # print (len(idname))
-        # http://yyyyy330.blog.163.com/album/#m=1&aid=268809115&p=1
-
         return self.album_url
 
     def get_album_purl(self):
-        self.album_purl = re.findall("purl: '(.*?)'", self.text)
+        self.album_purl = re.findall("purl:'(.*?)'", self.text)
         return self.album_purl
 
     def get_album_name(self):
@@ -91,11 +103,11 @@ class album(wangyiboke):
 
 
 class child_album(album):
-    def __init__(self, username, id):
+    def __init__(self, username, id, aid):
         album.__init__(self, username, id)
 
     def __str__(self):
-        self.album_url = 'http://yyyyy330.blog.163.com/album/#m=1&aid=227512529&p=1'
+        self.album_url = 'http://%s.blog.163.com/album/#m=1&aid=%s&p=1' % (self.username, self.aid)
         referer = 'http://%s.blog.163.com/album' % self.username
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
@@ -137,12 +149,13 @@ class pic(child_album):
 
 def main():
     B = album('yyyyy330', '134612310')
-    print(B.text)
+    print(B.get_album_aid())
+    # print(B.text)
+    # print (B.get_album_name())
+    # print (B.get_album_url())
 
-
-
-    C = child_album('yyyyy330', '134612310')
-    # print(C)
+    C = child_album('yyyyy330', '134612310', '261006686')
+    print(C.url)
 
 
 
