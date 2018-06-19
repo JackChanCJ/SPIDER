f0f5de8ce691477aa007b8be250e946e8e7be51c
JackChanCJ <408467753@qq.com>
2018-06-19 13:57:31 +0800

    POM_JIRA

---
+++ Demo/POM_JIRA/common/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+__author__ = 'JACK_CHAN'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- Demo/POM_JIRA/synapseRT NextGen.py
+++ Demo/POM_JIRA/synapseRT NextGen.py
@@ -1,11 +1,9 @@
 # coding=utf-8
 __author__ = 'JACK_CHAN'
 
-import os
 import xlrd
 from time import sleep
 from selenium import webdriver
-
 
 def open_jira():
     chromedriver = r"C:\Users\40846\AppData\Local\Google\Chrome\Application\chromedriver.exe"
@@ -16,14 +14,13 @@ def open_jira():
     driver.find_element_by_xpath('//*[@id="login-form-username"]').send_keys('jchen@incarcloud.com')
     driver.find_element_by_xpath('//*[@id="login-form-password"]').send_keys('000000')
     driver.find_element_by_xpath('//*[@id="login"]').click()
-
     sleep(10)
     driver.close()
     driver.quit()
 
 
 def read_excel():
-    fours = r"C:\Users\40846\Documents\Tencent Files\408467753\FileRecv\测试相关文档\测试案例\广汽三菱4S端系统测试测试用例.xls"
+    fours = r"C:\Users\40846\Desktop\自动化数据\车主APP.xlsx"
     data = xlrd.open_workbook(fours)
     table = data.sheets()
     for i in data.get_sheet_names():
@@ -39,8 +36,10 @@ def read_excel():
 
 
 def main():
+    open_jira()
+    read_excel()
     pass
 
 
 if __name__ == '__main__':
-    read_excel()
+    main()
